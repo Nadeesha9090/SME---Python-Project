f33ef3b6e37fc179a2dbb5da9e9f5cc299399517
Nadeesha Thilakaraathne <nadeesha9090@gmail.com>
2018-06-29 01:18:40 +0530

    Add Resarch Floder

---
+++ Research Code/Test 01 - Load CSV from Cloud and Linear Regression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 29 00:15:49 2018
+
+@author: hp
+"""
+
+import pandas as pd
+df = pd.read_csv('https://query.data.world/s/ueeueqq3wb5vxg2mm7ecc6kni4nm3z')
+
+#Print the headers of the dataset
+print(df.head())
